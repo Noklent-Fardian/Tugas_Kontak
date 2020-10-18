d92926dbadd3d404a75b47ee0fbda239affcf710
Noklent <70679702+Noklent@users.noreply.github.com>
2020-10-18 20:24:12 +0700

    Add files via upload

---
+++ koneksi_kontak.php
@@ -0,0 +1,10 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass ="";
+$nama_DB="kontak";
+$koneksi=mysqli_connect($host,$user,$pass,$nama_DB);
+if (!$koneksi) {
+	die ("Koneksi dengan datbase gagal:".mysql_connect_error());
+}
+?>
